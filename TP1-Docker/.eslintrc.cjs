ad5aab4896a94d8324bd16bff42d307220fe93d4
tonycava <anthony.cavge@gmail.com>
2024-03-29 15:47:37 +0100

    pipeline: add lint

--- TP1-Docker/.eslintrc.cjs
+++ TP1-Docker/.eslintrc.cjs
@@ -1,13 +1,10 @@
 module.exports = {
     "env": {
-        "es2021": true
+        "node": true
     },
     "extends": "eslint:recommended",
     "overrides": [
         {
-            "env": {
-                "node": true
-            },
             "files": [
                 ".eslintrc.{js,cjs}"
             ],
